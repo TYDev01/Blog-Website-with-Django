558a2e7638dcbef161346803e96bc426af5f59e5
Anthony <anthonychinaemeru84@yahoo.com>
2024-02-27 13:24:23 +0100

    Reversed the looped items, fixed the date also

--- full_blog/posts/views.py
+++ full_blog/posts/views.py
@@ -3,5 +3,5 @@ from .models import Posts
 
 # Create your views here.
 def posts(request):
-    posts = Posts.objects.all()
-    return render(request, 'posts/posts_list.html', {'post': posts})
+    posts = Posts.objects.all().order_by('-time')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
